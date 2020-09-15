dab702eb4e340f20bb84d9ab257aaff46fb95676
LuciDiscord <zkjcksn@gmail.com>
2020-09-15 22:06:29 +0000

    Github

--- src/end.js
+++ src/end.js
@@ -1,14 +1,14 @@
 module.exports = {
-    base: "https://tinee.gq/img/",
+    base: "https://tinee.gq/img",
     endpoints: {
-        ass: "https://tinee.gq/img/nsfw/ass/",
-        boobs: "https://tinee.gq/img/nsfw/boobs/",
-        feet: "https://tinee.gq/img/nsfw/feet/",
-        gifs: "https://tinee.gq/img/nsfw/gifs/",
-	hboobs: "https://tinee.gq/img/nsfw/hboobs/",
-	hentai: "https://tinee.gq/img/nsfw/hentai/",
-	kink: "https://tinee.gq/img/nsfw/kink/",
-	thighs: "https://tinee.gq/img/nsfw/thighs/",
-	yuri: "https://tinee.gq/img/nsfw/yuri/"
+        ass: "https://tinee.gq/img/nsfw/ass",
+        boobs: "https://tinee.gq/img/nsfw/boobs",
+        feet: "https://tinee.gq/img/nsfw/feet",
+        gifs: "https://tinee.gq/img/nsfw/gifs",
+	hboobs: "https://tinee.gq/img/nsfw/hboobs",
+	hentai: "https://tinee.gq/img/nsfw/hentai",
+	kink: "https://tinee.gq/img/nsfw/kink",
+	thighs: "https://tinee.gq/img/nsfw/thighs",
+	yuri: "https://tinee.gq/img/nsfw/yuri"
     }
 }
